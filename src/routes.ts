6eae287d40e2d6331e46f07ea4654e2a252b94e0
jouiwnl <jou.098olo@gmail.com>
2023-01-20 19:40:26 -0300

    minor fixes

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { FastifyInstance } from "fastify"
 import { z } from "zod"
 import { prisma } from "./lib/prisma"
 
+const weekDaysNumbers = [0, 1, 2, 3, 4, 5, 6] // Sun: 0, Sat: 6
+
 export async function appRoutes(app: FastifyInstance) {
   app.post('/habits', async (request) => {
     const createHabitBody = z.object({
@@ -89,6 +91,18 @@ export async function appRoutes(app: FastifyInstance) {
       where: {
         habit_id: id
       }
+    })
+
+    weekDaysNumbers.forEach(async dayNumber => {
+      if (weekDays.includes(dayNumber)) {
+        return;
+      }
+
+      await prisma.dayHabit.deleteMany({
+        where: {
+          habit_id: id
+        }
+      })
     })
 
     const habit = await prisma.habit.update({
@@ -305,6 +319,54 @@ export async function appRoutes(app: FastifyInstance) {
     })
 
     return year;
+  })
+
+  app.get('/get-range-date/:year', async (request) => {
+    const toggleParams = z.object({
+      year: z.coerce.number().min(1900).max(2999)
+    })
+
+    const { year } = toggleParams.parse(request.params);
+
+    const startDate = dayjs().year(year).startOf('year')
+    const currentYear = dayjs().startOf('year').get('year');
+    const isSameYear = currentYear === year;
+    const endDate = isSameYear ? new Date() : dayjs().year(year).endOf('year').toDate();
+  
+    let dateRange = []
+    let compareDate = startDate
+  
+    const weekDaysByStartDate = Array.from({ length: startDate.get('day') })
+      .map((_, index) => {
+        return {
+          index: index,
+          day_of_week: index + 1
+        }
+      });
+  
+    weekDaysByStartDate.reverse().forEach(day => {
+      const newDate = startDate.subtract(day.day_of_week, 'day');
+  
+      dateRange.push({
+        id: newDate.toISOString(),
+        date: newDate.toDate(),
+        day_of_week: startDate.get('day'),
+        disabled: true
+      })
+    })
+  
+    while (compareDate.isBefore(endDate)) {
+      dateRange.push({
+        id: compareDate.toISOString(),
+        date: compareDate.toDate(),
+        day_of_week: compareDate.get('day'),
+        disabled: false
+      })
+  
+      compareDate = compareDate.add(1, 'day')
+    }
+  
+    return dateRange
   })
 }
 
