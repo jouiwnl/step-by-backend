4df9c53e8680b3c115282cf55fbe626d62903210
João Henrique Leonor Martins <joao.martins@betha.com.br>
2023-04-07 20:07:04 -0300

    finishing coverage

--- src/test/controllers/dayController.spec.ts
+++ src/test/controllers/dayController.spec.ts
@@ -9,13 +9,43 @@ const app = fastify();
 app.register(appRoutes);
 
 describe('day controller test', () => {
-  test('get day', async () => {
+  test('get day with habit active', async () => {
+    const params = {
+      date: dayjs("2023-04-07").toDate(),
+      user_id: randomUUID()
+    }
+
+    const possibleHabit = getValidHabit(params.user_id);
+    const day = getValidDay(params.date, params.user_id, possibleHabit.id);
+    const validResponse = getValidCompleteDay([possibleHabit], day);
+
+    prisma.$queryRaw = vitest.fn().mockReturnValueOnce([possibleHabit]);
+    prisma.day.findFirst = vitest.fn().mockReturnValueOnce(day);
+    prisma.habit.findUnique = vitest.fn().mockReturnValueOnce(possibleHabit);
+
+    const res = await app.inject({
+      method: 'GET',
+      url: '/day',
+      query: {
+        date: params.date.toISOString(),
+        user_id: params.user_id
+      }
+    });
+
+    const json = await res.json();
+  
+    expect(json.completedHabits).toStrictEqual(validResponse.completedHabits);
+    expect(json.possibleHabits).toEqual(validResponse.possibleHabits);
+    expect(res.statusCode).toBe(200);
+  })
+
+  test('get day with habit without deactivation', async () => {
     const params = {
       date: new Date(),
       user_id: randomUUID()
     }
 
-    const possibleHabit = getValidHabit(params.user_id);
+    const possibleHabit = getValidHabitActive(params.user_id);
     const day = getValidDay(params.date, params.user_id, possibleHabit.id);
     const validResponse = getValidCompleteDay([possibleHabit], day);
 
@@ -71,6 +101,15 @@ function getValidHabit(user_id: string) {
     title: "Any habit",
     activation_date: dayjs("2023-04-07").toISOString(),
     deactivation_date: dayjs("2023-04-06").toISOString(),
+    created_at: dayjs().subtract(2, 'day').toISOString(),
+    user_id
+  }
+}
+
+function getValidHabitActive(user_id: string) {
+  return {
+    id: randomUUID(),
+    title: "Any habit",
     created_at: dayjs().subtract(2, 'day').toISOString(),
     user_id
   }
